5c62f6e106488682ca872b670c0c7ca722585df0
elBuitre <stefano.ontheroad@gmail.com>
2016-09-23 16:38:40 +0200

    First commit - freshly created project

---
+++ src/main/java/com/bui3/JasmineApplication.java
@@ -0,0 +1,12 @@
+package com.bui3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasmineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasmineApplication.class, args);
+	}
+}
