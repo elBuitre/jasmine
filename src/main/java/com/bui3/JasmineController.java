dcb90a605279a6be0d862d120092355df6d50d3c
elBuitre <stefano.ontheroad@gmail.com>
2016-09-28 14:42:51 +0200

    Home Page
    Implement a basic controller class and a home page to make the
    application working

---
+++ src/main/java/com/bui3/JasmineController.java
@@ -0,0 +1,15 @@
+package com.bui3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class JasmineController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String homePage() {
+		return "homePage";
+	}
+}
