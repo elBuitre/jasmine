6382d92c427d8b9edad8edf429767952a171e83b
elBuitre <stefano.ontheroad@gmail.com>
2017-01-27 13:43:50 +0100

    Pagination
    Implement the pagination for the users list in the administrator page

--- src/main/java/com/bui3/JasmineController.java
+++ src/main/java/com/bui3/JasmineController.java
@@ -3,10 +3,15 @@ package com.bui3;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,31 +20,70 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/")
 public class JasmineController {
 
+	@Value("${users-list-size}")
+	private int usersListSize;
+	
+	@Value("${pager-size}")
+	private int pagerSize;
+
 	@Autowired
 	UserRepository userRepository;
 	
 	@Autowired
 	RoleRepository roleRepository;
 	
-	@ModelAttribute("users")
+	//@ModelAttribute("users")
+	public Page<User> populateUsers(Integer pageNumber) {
+		PageRequest request =
+				new PageRequest(pageNumber-1, usersListSize,
+						Sort.Direction.ASC, "username");
+		return userRepository.findAll(request);
+	}
+	/*
 	public List<User> populateUsers() {
 		return userRepository.findAll();
 	}
+	*/
 	
 	@ModelAttribute("roles")
 	public List<Role> populateRoles() {
 		return roleRepository.findAll();
 	}
 
+	
+	
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public String homePage() {
 		return "homePage";
 	}
 	
-	@RequestMapping(value="/admin", method=RequestMethod.GET)
-	public String adminPage(User user) {
+	
+	
+	
+	@RequestMapping(value="/admin{pageNumber}", method=RequestMethod.GET)
+	public String adminPage(@PathVariable Integer pageNumber, Model model,
+			User user) {
+		
+		Page<User> page = populateUsers(
+				((null != pageNumber) ? pageNumber : 1));
+		
+		int pages = page.getTotalPages();
+		int current = page.getNumber() + 1;
+		int begin = Math.max(1, current - (pagerSize - 1));
+		int end = Math.min(begin + (pagerSize - 1), pages);
+
+		model.addAttribute("users", page);
+		model.addAttribute("beginIndex", begin);
+		model.addAttribute("endIndex", end);
+		model.addAttribute("currentIndex", current);
+		model.addAttribute("totalPages", pages);
+		
 		return "adminPage";
 	}
+
+	
+	
 	
 	@RequestMapping(value="/admin", method=RequestMethod.POST)
 	public String addUser(User user, BindingResult bindingResult) {
@@ -50,6 +94,9 @@ public class JasmineController {
 		userRepository.save(user);
 		return "redirect:/admin";
 	}
+	
+	
+	
 	
 	@RequestMapping(value="/admin/delete", method=RequestMethod.GET)
 	public String deleteUser(
@@ -72,6 +119,9 @@ public class JasmineController {
 		return "redirect:/admin";
 	}
 	
+	
+	
+	
 	@RequestMapping(value="/admin/edit", method=RequestMethod.GET)
 	public String editUser(
 			@RequestParam(value="username", required=true) String username,
@@ -85,6 +135,9 @@ public class JasmineController {
 		}
 		return "redirect:/admin";
 	}
+	
+	
+	
 	
 	@RequestMapping(value="/admin/edit", method=RequestMethod.POST)
 	public String editingUser(
